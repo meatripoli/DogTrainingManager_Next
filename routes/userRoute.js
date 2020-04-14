6373a997a6b341658c84f0632f3bdf3e6a7d2d37
Maria Elena Tripoli <meatripoli@gmail.com>
2020-04-14 16:09:14 -0500

    typo fixes

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -7,21 +7,17 @@ module.exports = (handle,server) => {
     server.use(bodyParser.urlencoded({ extended: false }))
     server.use(bodyParser.json());
     server.post('/login',(req,res)=>{
-        console.log('inside post /login',req.body)  
-        //check bcrypt
-        //checkdatabase to see if user and password match
         db.user.findOne({
             where: {
                 user: req.body.user
             }
-        }).then((queryResp)=>{      
-            console.log('query response:',queryResp)   
+        }).then((queryResp)=>{    
+            //check password provided against password in database  
             bcrypt.compare(req.body.password, queryResp.password, (err, response) => {
                 if (err) {
                     console.error(err)
                     res.status(401);
                 }
-                console.log(response) //true or false
                 response?
                 res.status(200).json({
                     success:true,
@@ -36,33 +32,6 @@ module.exports = (handle,server) => {
                     data: ''
                 });
             });  
-            // if(queryResp.password === req.body.password && queryResp.adminflag){
-            //     console.log('admin')
-            //     //send to profile page once authenticated
-            //     res.status(200).json({
-            //       success:true,
-            //       redirectUrl: '/dogs',
-            //       data: queryResp
-            //   })
-            // }
-            // else if(queryResp.password === req.body.password && !queryResp.adminflag){
-            //     console.log('lowlife')
-            //     //send to profile page once authenticated
-            //     res.status(200).json({
-            //       success:true,
-            //       redirectUrl: '/dogs',
-            //       message: 'password correct',
-            //       data: queryResp
-            //   })
-            // }
-            // else{
-            //     res.status(401).json({
-            //         success:false,
-            //         redirectUrl: '/',
-            //         message: 'password incorrect',
-            //         data: ''
-            //     })
-            // };
         }).catch((error) => {
             res.status(401);
             res.json({error:error, stackError:error.stack});
